bd81a0339fa98fc9e10ca082a1cf4d5b94c70ad5
Diego Pennisi <diegopennisi@outlook.es>
2019-04-09 15:04:52 +0200

    web terminada y montada

---
+++ indexphp.php
@@ -0,0 +1,18 @@
+<?php
+	try {
+	$conexion = new PDO('mysql:host=localhost;dbname=franweb','root', '' );
+	$conexion->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+	$conexion->exec('SET CHARACTER SET utf8');
+ 
+	} catch (Exception $e) {
+		echo "EL ERROR ESTA EN LA LINEA" .$e->getLine();
+    }
+    
+  
+     $sql =$conexion->prepare('SELECT * FROM header');
+      $sql->execute();
+     $res = $sql->fetchAll();
+    
+     
+
+ ?>
